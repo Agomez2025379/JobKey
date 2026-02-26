0114e5e812bf7cbdc13c5b3cf929f7dc1d732b02
Michaelby2008 <maquino-2025436@kinal.edu.gt>
2026-02-26 15:43:59 -0600

    Cree la entidad de Candidatos y Empresas agregando todas las entidades de la base de datos y colocando departamentos_id como llave foránea hacia la clase Departamentos.

---
+++ jobKey/jobKey/src/main/java/com/crusaders/jobKey/entity/Candidatos.java
@@ -0,0 +1,75 @@
+package com.crusaders.jobKey.entity;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter //utilizo ambas anotaciones para no tener q colocar manualmente los getter and setters
+@JsonPropertyOrder({
+        "id_candidato",
+        "nombre",
+        "apellido",
+        "email",
+        "telefono",
+        "password_hash",
+        "profesion",                      // Coloco el orden que tengo en mi tabla
+        "experiencia",
+        "educacion",
+        "habilidades",
+        "curriculum_url",
+        "departamento_id",
+        "fecha_registro"})
+@Entity
+@Table( name = "candidatos")
+public class Candidatos {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_candidato")
+    private Integer idCandidato;
+
+    @Column(name = "nombre")
+    private String nombre;
+
+    @Column(name = "apellido")
+    private String apellido;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "telefono")
+    private String telefono;
+
+    @JsonIgnore //Para que no se envie al usuario
+    @Column(name = "password_hash")
+    private String passwordHash;
+
+    @Column(name = "profesion")
+    private String profesion;
+
+    @Column(name = "experiencia")
+    private String experiencia;
+
+    @Column(name = "educacion")
+    private String eduacion;
+
+    @Column(name = "habilidades")
+    private String habilidades;
+
+    @Column(name = "curriculum_url")
+    private String curriculumUrl;
+
+    @ManyToOne // Para hacerlo llave foranea utilizo la relacion muchos a uno
+    @JoinColumn(name = "departamento_id") // esto es para decirle q busque
+    private Departamentos departamentos;
+
+    @Column(name = "fecha_registro")
+    private LocalDateTime fechaRegistro;
+
+}
---
+++ jobKey/jobKey/src/main/java/com/crusaders/jobKey/entity/Empresas.java
@@ -0,0 +1,64 @@
+package com.crusaders.jobKey.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter //utilizo ambas anotaciones para no tener q colocar manualmente los getter and setters
+@JsonPropertyOrder({
+        "id_empresa",
+        "nombre_empresa",
+        "email",
+        "telefono",
+        "password_hash",
+        "descripcion",               // Coloco el orden que tengo en mi tabla
+        "sector_empresarial",
+        "logo_url",
+        "departamento_id",
+        "fecha_registro"
+})
+@Entity
+@Table( name = "empresas")
+public class Empresas {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_empresa")
+    private Integer idEmpresa;
+
+    @Column(name = "nombre_empresa")
+    private String nombreEmpresa;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "telefono")
+    private String telefono;
+
+    @JsonIgnore //Para que no se envie al usuario
+    @Column(name = "password_hash")
+    private String passwordHash;
+
+    @Column(name = "descripcion")
+    private String descripcion;
+
+    @Column(name = "sector_empresarial")
+    private String  sectorEmpresarial;
+
+    @Lob
+    @Column(name = "logo_url", columnDefinition = "LONGBLOB")
+    private byte[] logoUrl;
+
+    @ManyToOne // Utilizo esto para hacerlo llave foranea
+    @JoinColumn(name = "departamento_id") // esto es para decirle q busque departamento_id
+    private Departamentos departamentos; // Le asignamos como la clase Departamentos para q sepa q ahi debe buscar
+
+    @Column(name = "fecha_registro")
+    private LocalDateTime fechaRegistro;
+
+}
