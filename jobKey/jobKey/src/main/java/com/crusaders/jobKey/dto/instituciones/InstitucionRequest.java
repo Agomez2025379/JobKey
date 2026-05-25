7c236a1663cbfe01f9d2296870a98133f49aa7f9
Michaelby2008 <maquino-2025436@kinal.edu.gt>
2026-05-24 20:59:44 -0600

    Arreglo de problema de los dto mayuscula

--- jobKey/jobKey/src/main/java/com/crusaders/jobKey/dto/instituciones/InstitucionRequest.java
+++ jobKey/jobKey/src/main/java/com/crusaders/jobKey/dto/instituciones/InstitucionRequest.java
@@ -8,11 +8,8 @@ import lombok.Setter;
 @Setter
 public class InstitucionRequest {
 
-<<<<<<< HEAD
-=======
     private Integer idInstitucion;
     private String email;
->>>>>>> edc3e7a67fd99a8f64f281b97ff1efb37eaa63db
     private Integer usuarioId;
     private String nombreInstitucion;
     private String telefono;
